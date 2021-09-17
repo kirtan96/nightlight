ff84d75d5bb429a4fecbf4237e4dbab871ae0489
Kirtan Patel <kirtan96@gmail.com>
2021-09-16 21:25:38 -0700

    Updates

---
+++ precache-manifest.a258263e634a9470832c96e624e9e7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f1382a04088dea6503eac4760cb2c3",
+    "url": "/nightlight/index.html"
+  },
+  {
+    "revision": "417b570cefdbf3237a95",
+    "url": "/nightlight/static/css/main.3d9d07bb.chunk.css"
+  },
+  {
+    "revision": "2bd9e36dad297dab80c8",
+    "url": "/nightlight/static/js/2.d1596f33.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/nightlight/static/js/2.d1596f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417b570cefdbf3237a95",
+    "url": "/nightlight/static/js/main.46a832ee.chunk.js"
+  },
+  {
+    "revision": "5505fb2cf362c69176e3",
+    "url": "/nightlight/static/js/runtime-main.80dff1b4.js"
+  }
+]);
